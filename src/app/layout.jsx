959053996b2fa9438ba76a792b13536f8151ac16
yung bunnarith <bunreth552@gmail.com>
2026-03-25 22:50:31 +0700

    sideBar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,16 +2,22 @@ import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar"
 import { AppSidebar } from "../components/app-sidebar"
 import { TooltipProvider } from "../components/ui/tooltip"
 import "./globals.css"
+import {Noto_Sans} from "next/font/google"
+
+const notoSan = Noto_Sans({
+  subsets: ["latin"],
+  weight: ['400', '700']
+})
 
 export default function Layout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={notoSan.className}>
         <TooltipProvider>
           <SidebarProvider>
             <AppSidebar />
             <main>
-              <SidebarTrigger />
+              <SidebarTrigger className="hover:cursor-pointer" />
               {children}
             </main>
           </SidebarProvider>
